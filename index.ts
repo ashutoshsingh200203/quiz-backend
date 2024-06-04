f6655cb2bc7e56b95802812d527f4a33c840d86c
ashutoshsingh200203 <ashutoshsingh.tomar.2024@gmail.com>
2024-06-04 19:07:46 +0530

    try to implement chat communication with socket

--- index.ts
+++ index.ts
@@ -2,10 +2,16 @@ import express, { Request, Response } from 'express';
 import './config/database' ;
 import bodyParser from 'body-parser';
 import authRoutes from './routes/authRoutes'
+import {Server} from 'socket.io'
+
+
 const cors = require('cors');
 require('dotenv').config();
 const app = express() ;
-app.use(cors())
+
+app.use(cors({
+  origin: "*" 
+}))
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
@@ -15,7 +21,21 @@ app.get('/',(req : Request,res : Response)=>{
   res.json('hello')
 })
 
-app.listen(5000,()=>{
+ const server = app.listen(5000,()=>{
   console.log("server running successfully")
 })
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true
+  }
+})
+
+io.on('connection',(socket)=>{
+  socket.emit('hello' , 'world')
+
+  socket.on('howdy',(arg)=>{
+    console.log(arg) ;
+  })
+})
